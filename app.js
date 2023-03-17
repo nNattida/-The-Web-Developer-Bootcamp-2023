b7336720f79a013a9ac59ad24f1ecd03d68a791c
nNattida <94751812+nNattida@users.noreply.github.com>
2023-03-18 00:58:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
+let input = prompt('What would you like to do ?');
+const todo = [];
 
+while(input!=='quit' && input!=='q'){
+    if(input === 'list'){
+        console.log('***************');
+        for(let i = 0; i < todo.length; i++){
+            console.log(`${i}: ${todo[i]}`);
+        }
+        console.log('***************');
+    } else if(input === 'new'){
+        const newTodo = prompt('What is the new to do');
+        todo.push(newTodo);
+        console.log(`${newTodo} was added to the list`);
+    } else if(input === 'delete'){
+        const index = parseInt(prompt('What index do you want to delete ?'));
+        if(!Number.isNaN(index)){
+            console.log(`${todo[index]} was deleted from the list`);
+            todo.splice(index, 1);
+        } 
+        else{
+            console.log('Unknown index');
+        }
+    }
+    input = prompt('What would you like to do ?');
+}
+
+console.log('Quit!');
