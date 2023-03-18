99ba547d119f5a670fc23c2a7ba17410efb6e574
nNattida <94751812+nNattida@users.noreply.github.com>
2023-03-18 23:48:31 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-
+// DEFINE YOUR FUNCTION BELOW:
+function returnDay(numDay){
+    const weekDay = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+    
+    if(numDay >= 1 && numDay <= 7){
+        return weekDay[numDay-1];
+    }
+    return null;
+}
