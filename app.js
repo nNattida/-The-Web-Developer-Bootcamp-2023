453810c894f2ecc5efb74939609c03d5edc1c492
nNattida <94751812+nNattida@users.noreply.github.com>
2023-03-18 23:00:48 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-
+// define isSnakeEyes below:
+function isSnakeEyes(firstNum , secondNum){
+    if (firstNum === 1 && secondNum === 1){
+        console.log('Snake Eyes!')
+    } else{
+        console.log('Not Snake Eyes!')
+    }
+}
